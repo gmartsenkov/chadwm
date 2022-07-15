464a011b5d7878fd65fd39a97dcaa7f6ce105409
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-07-15 11:25:48 +0100

    Adds spotify statusbar widget

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -75,10 +75,30 @@ weather() {
   printf "^b$grey^^c$white^ $temp ^d^"
 }
 
+spotify() {
+   playstatus=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus'|egrep -A 1 "string"|cut -b 26-|cut -d '"' -f 1|egrep -v ^$)
+   # artist=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+   # album=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "album"|egrep -v "album"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+   title=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$)
+
+   if [ "$playstatus" = "Playing" ]; then
+      result="^b$darkblue^^c$grey^  $title ^d^"
+   else
+      result="^b$darkblue^^c$grey^  $title ^d^"
+   fi
+
+  printf "$result"
+}
+
 while true; do
 
   [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates) && weather=$(weather)
   interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$updates $weather $(cpu) $(mem) $(wlan) $(volume) $(clock)"
+  spotifypid="$(pidof -s spotify || pidof -s .spotify-wrapped)"
+  if [ -z "$spotifypid"]; then
+     sleep 1 && xsetroot -name "$updates $weather $(cpu) $(mem) $(wlan) $(volume) $(clock)"
+  else
+     sleep 1 && xsetroot -name "$updates $weather $(spotify) $(cpu) $(mem) $(wlan) $(volume) $(clock)"
+  fi
 done
